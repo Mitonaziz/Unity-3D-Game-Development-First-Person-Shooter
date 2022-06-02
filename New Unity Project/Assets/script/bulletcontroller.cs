57e82f06215d31fd20f911c88985f9cc87c80585
Azizur <106616550+Mitonaziz@users.noreply.github.com>
2022-06-02 21:12:06 +0600

    I update camera movement and use gun

---
+++ New Unity Project/Assets/script/bulletcontroller.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletcontroller : MonoBehaviour
+{
+    public float speed;
+
+    public Rigidbody myRigidbody;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        bulletfluy();
+    }
+
+    private void bulletfluy()
+    {
+        myRigidbody.velocity = transform.forward * speed;
+    }
+}
--- New Unity Project/Assets/script/player.cs
+++ New Unity Project/Assets/script/player.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 12.5f;
+    public float mousespeed = 100f;
+    public CharacterController mycontroller;
+    
+    
+    
+   // public Quaternion cameraVerticalRotation;
+  
+    
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        playermovementkey();
         
+       // playermovementmous();
+       
+        
+    }
+    
+
+    void playermovementmous()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mousespeed * Time.deltaTime;
+        transform.Rotate(Vector3.up * mouseX);
+        //camera up and down
+        //mouseY = Input.GetAxis("Mouse Y");
+
+
+
+
+        //float mouseY = Input.GetAxis("Mouse Y") * mousespeed * Time.deltaTime;
+        //transform.Rotate(Vector3.down * mouseY);
+
+        // cameraVerticalRotation -=mouseY;
+        //myCamerahead.localPosition =  Quaternion.Euler(cameraVerticalRotation, 0f, 0f);
+
+    }
+
+    void playermovementkey()
+    {
+        {
+            //you go to edit project settings and axis
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+            Vector3 movement = x * transform.right + z * transform.forward;
+            movement = movement * speed * Time.deltaTime;
+            mycontroller.Move(movement);
+
+
+
+        }   
     }
 }
